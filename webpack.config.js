6ae642463d2de6665a8c89033240343d706e3b29
ydj <yuandj@channelsoft.com>
2017-03-29 18:33:11 +0800

    使用模板生成访问页面，添加indexTemp.html模板，在配置文件中配置生成的页面文件为indexDev.html;此时浏览器中输入localhost:8080/indexDev.html即可访问

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
 /**
  * Created by Yuan on 2017/3/29.
  */
-var  webpack = require('webpack');
+var webpack = require('webpack');
 var path = require('path');//引入node的path库
 
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
     //入口文件
-    entry:{
-        app:['./app/index.js'],
+    entry: {
+        app: ['./app/index.js'],
     },
     output: {
         path: path.resolve(__dirname, './'),//指定编译后的代码位置为/bundle.js
-        publicPath:"/",
+        publicPath: "/",
         filename: 'bundle.js'
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            title:'devPage'
+            title: 'devPage',
+            filename: 'indexDev.html',   //设置这个html的文件名
+            template: 'indexTemp.html', //要使用的模块的路径
+            inject: 'body', //把模板注入到哪个标签后
+            minify: false, //是否压缩
+            hash: true, //是否hash化
+            cache: false, //是否缓存
+            showErrors: false //是否显示错误
         }),
 
     ],
